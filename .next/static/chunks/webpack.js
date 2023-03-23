a973209a11eabfd10c3941bb5deb594800b56a70
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2023-03-23 08:58:41 -0700

    update icons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "81daaba820510f13"; }
+/******/ 		__webpack_require__.h = function() { return "88703319e91e91e4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
