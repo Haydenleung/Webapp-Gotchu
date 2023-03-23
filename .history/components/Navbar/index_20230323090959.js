92599049a61dfe0a49aacac7b78b2b0ec6348786
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2023-03-23 09:31:35 -0700

    add team css & fix icons

---
+++ .history/components/Navbar/index_20230323090959.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react'
+import styles from "./Navbar.module.css"
+import Image from 'next/image'
+import { useState } from 'react'
+
+export default function Navbar() {
+
+  const [popup, setPopup] = useState(false);
+  const [reverse, setReverse] = useState();
+
+  useEffect(() => {
+    reverse === true && popup === true ? (setPopup(false), setReverse(false)) : <></>;
+    reverse === true && popup === false ? (setPopup(true), setReverse(false)) : <></>;
+  })
+
+  return (
+    <section className={styles.navbar}>
+      <div className={styles.headbar}>
+        <Image src={'NavbarLogo.svg'} className={styles.navbarLogo} width={40} height={40} />
+        <Image src={'hamburger.svg'} onClick={() => setReverse(true)} className={styles.hamburger} width={40} height={40} />
+      </div>
+      {
+        popup === true ?
+          <div className={styles.extendbar}>
+            <div className={styles.extendtab}>
+              <Image src={'/icons/home.svg'} className={styles.hamburger} width={20} height={20} />
+              <h4>Home</h4>
+            </div>
+            <div className={styles.extendtab}>
+              <Image src={'/icons/quiz.svg'} className={styles.hamburger} width={20} height={20} />
+              <h4>Quiz</h4>
+            </div>
+            <div className={styles.extendtab}>
+              <Image src={'/icons/result.svg'} className={styles.hamburger} width={20} height={20} />
+              <h4>Result</h4>
+            </div>
+          </div>
+          : <></>
+      }
+    </section>
+  )
+}
