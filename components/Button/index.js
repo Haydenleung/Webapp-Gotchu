793032e22dcf08ce51525d957a1cf8c1b6c3c595
kionale <kiona.peng@gmail.com>
2023-03-23 16:03:18 -0700

    option components and question page added

--- components/Button/index.js
+++ components/Button/index.js
@@ -2,15 +2,15 @@ import React from 'react'
 import styles from './Button.module.css'
 
 export default function Button({
-    text="",
-    color=purple,
-    width='349'
+    text = "",
+    color = purple,
+    width = 349
 
 }) {
     return (
         <div>
             <button className={styles.button}
-                style={{ backgroundColor: `${color}`, width:{ width } }}>
+                style={{ backgroundColor: `${color}`, width: { width } }}>
                 {text}
             </button>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -33,8 +33,13 @@ export default function Home() {
               </Link>
               <Link href='/introduction'>
                 {/* <button className={styles.actionBtn}>Get Started</button> */}
+<<<<<<< HEAD
                 <Button text="Get Started" color='#896686' width='180' />
               </Link>
+=======
+                <Button text="Get Started" color='#896686' width={180} />
+            </Link>
+>>>>>>> 11bff50 (option components and question page added)
             </div>
           </div>
 
