e18c152d1189431938e185bcf31c14522a0c9c23
kionale <kiona.peng@gmail.com>
2023-04-03 16:47:45 -0700

    introduction slide updated

--- components/Button/index.js
+++ components/Button/index.js
@@ -4,13 +4,13 @@ import styles from './Button.module.css'
 export default function Button({
     text = "",
     color = purple,
-    width = 349
+    
 
 }) {
     return (
         <div>
             <button className={styles.button}
-                style={{ backgroundColor: `${color}`, width: { width } }}>
+                style={{ backgroundColor: `${color}`}}>
                 {text}
             </button>
         </div>
