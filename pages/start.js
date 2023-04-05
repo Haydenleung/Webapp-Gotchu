ee93493b506c93d74e36a0667a1148ec9f39d973
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2023-04-05 16:38:05 -0700

    home page done, introduction card revised, start page updated

--- pages/start.js
+++ pages/start.js
@@ -4,6 +4,7 @@ import styles from "../styles/Start.module.css"
 import Link from 'next/link'
 import Navbar from '@/components/Navbar'
 import BackButton from '@/components/BackButton'
+import { motion } from 'framer-motion'
 
 export default function Start() {
     return (
@@ -17,8 +18,29 @@ export default function Start() {
                             <h4>Hi </h4>
                         </div>
                         {/* Replace by Aniamte Shape Components */}
-                        <div className={styles.shape}>
-                        </div>
+                        <Link href="/questionpage" className={styles.shape}>
+                            <motion.div
+                                className={styles.box}
+                                initial={{ borderRadius: "70% 60% 45% 68% / 68% 68% 35% 38%", rotate: 0 }}
+                                whileTap={{
+                                    scale: [1, 0.6],
+                                    rotate: [0, 90],
+                                    height: ["22rem", "28rem"],
+
+                                    borderRadius: ["70% 60% 45% 68% / 68% 68% 35% 38%", "99% 99% 99% 99% / 99% 99% 99% 99%"]
+                                }}
+                                transition={{
+                                    duration: 15,
+                                    // ease: "easeInOut",
+                                    ease: "linear"
+                                }}
+                            />
+                            <p className={styles.shapeText}>Keep Pressing and
+                                take a Deep Breathe<br /><br />
+                                Release your finger
+                                when you are Ready to Go</p>
+                        </Link>
+
                     </div>
                 </div>
                 <div className={styles.bottomGraphic}>
