d45a481ff4383f1ee8cacffc6915805eebcc4ba1
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2023-04-06 16:33:17 -0700

    add animation, fixed pages

--- pages/personal.js
+++ pages/personal.js
@@ -1,4 +1,5 @@
-import { React } from 'react'
+import { React, useEffect } from 'react'
+import Head from 'next/head'
 import { useState } from 'react'
 import styles from "../styles/Personal.module.css"
 import Link from 'next/link'
@@ -8,63 +9,110 @@ import NextButton from '@/components/NextButton'
 import ColorSelection from '@/components/ColorSelection'
 
 export default function Personal() {
+
+    const [purple, setPurple] = useState(false);
+    const [green, setGreen] = useState(false);
+    const [orange, setOrange] = useState(false);
+    const [red, setRed] = useState(false);
+    const [brown, setBrown] = useState(false);
+    const [tan, setTan] = useState(false);
+
+    const [imgP, setImgP] = useState();
+    const [imgG, setImgG] = useState();
+    const [imgO, setImgO] = useState();
+    const [imgR, setImgR] = useState();
+    const [imgB, setImgB] = useState();
+    const [imgT, setImgT] = useState();
+
+    const colorChange = () => {
+        setPurple(false);
+        setGreen(false);
+        setOrange(false);
+        setRed(false);
+        setBrown(false);
+        setTan(false);
+    }
+
+    useEffect(() => {
+        purple === true ? setImgP('/character/purple_character_select.svg') : setImgP('/character/purple_character.svg')
+        green === true ? setImgG('/character/green_character_select.svg') : setImgG('/character/green_character.svg')
+        orange === true ? setImgO('/character/orange_character_select.svg') : setImgO('/character/orange_character.svg')
+        red === true ? setImgR('/character/red_character_select.svg') : setImgR('/character/red_character.svg')
+        brown === true ? setImgB('/character/brown_character_select.svg') : setImgB('/character/brown_character.svg')
+        tan === true ? setImgT('/character/tan_character_select.svg') : setImgT('/character/tan_character.svg')
+    })
+
     const [name, setName] = useState('');
     const handleInputChange = (e) => {
         // Remove any non-alphabetic characters using regex
-        const inputValue = e.target.value.replace(/[^a-zA-Z]/g, '');
+        const inputValue = e.target.value.replace(/[^a-zA-Z" "]/g, '');
         // Update the input field's value
         e.target.value = inputValue;
         setName(e.target.value);
     };
 
     return (
-        <div className={styles.main}>
-            <div className={styles.empty} />
-            <Navbar />
-            <div className={styles.main_container}>
-                <div className={styles.pageContent}>
-                    <div className={styles.topContent}>
-                        <div className={styles.pageTitle}>
-                            <h4>What's your name?</h4>
-                            <p>(Optional)</p>
-                        </div>
+        <>
+            <Head>
+                <title>Get Started</title>
+                <meta name="description" content="Anti-Bully App" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={styles.main}>
+                <div className={styles.empty} />
+                <Navbar />
+                <div className={styles.main_container}>
+                    <div className={styles.pageContent}>
+                        <div className={styles.topContent}>
+                            <div className={styles.pageTitle}>
+                                <h4>What's your name?</h4>
+                                <p>(Optional)</p>
+                            </div>
+                            <div className={styles.nameInput}>
+                                <input
+                                    type="text"
+                                    value={name}
+                                    placeholder="Characters Only..."
+                                    onChange={handleInputChange}
+                                    className={styles.inputFelid}
+                                />
+                            </div>
+                            <div className={styles.pagePick}>
+                                <h4>Pick a color you like</h4>
+                            </div>
+                            <div className={styles.character}>
+                                <div className={styles.colorTile} onClick={() => { colorChange(); setPurple(true) }}>
+                                    <ColorSelection img={imgP} />
+                                </div>
+                                <div className={styles.colorTile} onClick={() => { colorChange(); setGreen(true) }}>
+                                    <ColorSelection img={imgG} />
+                                </div>
+                                <div className={styles.colorTile} onClick={() => { colorChange(); setOrange(true) }}>
+                                    <ColorSelection img={imgO} />
+                                </div>
+                                <div className={styles.colorTile} onClick={() => { colorChange(); setRed(true) }}>
+                                    <ColorSelection img={imgR} />
+                                </div>
+                                <div className={styles.colorTile} onClick={() => { colorChange(); setBrown(true) }}>
+                                    <ColorSelection img={imgB} />
+                                </div>
+                                <div className={styles.colorTile} onClick={() => { colorChange(); setTan(true) }}>
+                                    <ColorSelection img={imgT} />
+                                </div>
+                            </div>
 
-                        <div className={styles.nameInput}>
-                            <input
-                                type="text"
-                                value={name}
-                                placeholder="Characters Only"
-                                onChange={handleInputChange}
-                                className={styles.inputFelid}
-                            />
                         </div>
-                        <div className={styles.pagePick}>
-                            <h4>Pick a color you like</h4>
-                        </div>
+                        <div className={styles.bottomContent}>
 
-                        <div className={styles.colorTile}>
-                            <ColorSelection img='purple_character.png' />
-                            <ColorSelection img='green_character.png' />
+                            <Link href={{ pathname: "/start", query: { name } }}>
+                                <NextButton />
+                            </Link>
                         </div>
-                        <div className={styles.colorTile}>
-                            <ColorSelection img='orange_character.png' />
-                            <ColorSelection img='red_character.png' />
-                        </div>
-                        <div className={styles.colorTile}>
-                            <ColorSelection img='brown_character.png' />
-                            <ColorSelection img='tan_character.png' />
-                        </div>
-                        
-                    </div>
-                    <div className={styles.bottomContent}>
-
-                        <Link href="/start">
-                            <NextButton />
-                        </Link>
                     </div>
                 </div>
+                <div className={styles.empty} />
             </div>
-            <div className={styles.empty} />
-        </div>
+        </>
     )
 }
--- pages/start.js
+++ pages/start.js
@@ -1,58 +1,110 @@
 import React from 'react'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import styles from "../styles/Start.module.css"
 import Link from 'next/link'
+import Image from 'next/image'
 import Navbar from '@/components/Navbar'
 import BackButton from '@/components/BackButton'
 import { motion } from 'framer-motion'
 
 export default function Start() {
-    return (
-        <div className={styles.main}>
-            <div className={styles.empty} />
-            <Navbar />
-            <div className={styles.main_container}>
-                <div className={styles.pageContent}>
-                    <div className={styles.topContent}>
-                        <div className={styles.pageTitle}>
-                            <h4>Hi </h4>
-                        </div>
-                        {/* Replace by Aniamte Shape Components */}
-                        <Link href="/questionpage" className={styles.shape}>
-                            <motion.div
-                                className={styles.box}
-                                initial={{ borderRadius: "70% 60% 45% 68% / 68% 68% 35% 38%", rotate: 0 }}
-                                whileTap={{
-                                    scale: [1, 0.6],
-                                    rotate: [0, 90],
-                                    height: ["22rem", "28rem"],
 
-                                    borderRadius: ["70% 60% 45% 68% / 68% 68% 35% 38%", "99% 99% 99% 99% / 99% 99% 99% 99%"]
-                                }}
+    const router = useRouter();
+    const query = router.query;
+    const name = query.name;
+
+    return (
+        <>
+            <Head>
+                <title>Get Started</title>
+                <meta name="description" content="Anti-Bully App" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={styles.main}>
+                <div className={styles.empty} />
+                <Navbar />
+                <div className={styles.main_container}>
+                    <div className={styles.pageContent}>
+                        <div className={styles.topContent}>
+                            <div className={styles.pageTitle}>
+                                <h4>Hello <span>{name}</span></h4>
+                            </div>
+                            {/* Replace by Aniamte Shape Components */}
+                            <Link href="/questionpage" className={styles.shape}>
+                                <motion.div
+                                    className={styles.box}
+                                    initial={{ borderRadius: "70% 60% 45% 68% / 68% 68% 35% 38%", rotate: 0 }}
+                                    whileTap={{
+                                        scale: [1, 0],
+                                        rotate: [0, 90],
+                                        height: ["24rem", "28rem"],
+
+                                        borderRadius: ["70% 60% 45% 68% / 68% 68% 35% 38%", "99% 99% 99% 99% / 99% 99% 99% 99%"]
+                                    }}
+                                    transition={{
+                                        duration: 15,
+                                        // ease: "easeInOut",
+                                        ease: "linear"
+                                    }}
+                                />
+                                <p className={styles.shapeText}><span>Keep Pressing</span> and
+                                    take a Deep Breathe<br /><br />
+                                    <span>Release</span> your finger
+                                    when you are Ready to Go</p>
+                            </Link>
+
+                        </div>
+                    </div>
+                    <div className={styles.bottomGraphic}>
+                        {/* Replace by Animated Graphic and Buttons Components */}
+                        <motion.img
+                            src={'/walkingBG.svg'}
+                            className={styles.walkingImg}
+                            width={1000}
+                            height={1000}
+                            initial={{ x: 650 }}
+                            animate={{ x: -100 }}
+                            transition={{
+                                duration: 12.5,
+                                ease: "linear"
+                            }}
+                        />
+                        <motion.div
+                            className={styles.walkingCharacter}
+                            transition={{
+                                duration: 6,
+                            }}
+                            animate={{
+                                x: ["-120%", "120%"],
+                            }}
+                        >
+                            <motion.img
+                                src={'/character/walkingHugo.svg'}
+                                width={80}
+                                height={80}
                                 transition={{
-                                    duration: 15,
-                                    // ease: "easeInOut",
-                                    ease: "linear"
+                                    duration: 0.5,
+                                    repeat: 24,
+                                    repeatType: "reverse",
+                                    ease: "easeIn"
+                                }}
+                                initial={{ y: "10%" }}
+                                animate={{
+                                    y: ["-30%", "10%"],
                                 }}
                             />
-                            <p className={styles.shapeText}>Keep Pressing and
-                                take a Deep Breathe<br /><br />
-                                Release your finger
-                                when you are Ready to Go</p>
-                        </Link>
-
+                        </motion.div>
+                        <div>
+                            {/* <Link href="/personal">
+                                <BackButton />
+                            </Link> */}
+                        </div>
                     </div>
                 </div>
-                <div className={styles.bottomGraphic}>
-                    {/* Replace by Animated Graphic and Buttons Components */}
-                    <div className={styles.bottomContent}>
-                        <Link href="/personal">
-                            <BackButton />
-                        </Link>
-                    </div>
-                </div>
+                <div className={styles.empty} />
             </div>
-            <div className={styles.empty} />
-        </div>
+        </>
     )
 }
